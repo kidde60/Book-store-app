7f54ec31c5bc2e9e5254536702e10f9172d891d3
kidde60 <kgeorgewilliam60@gmail.com>
2022-09-24 17:27:30 +0300

    Addhome page

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import AddBook from './AddBook';
+import Book from './Book';
+
+const Home = () => (
+  <div className="Home">
+    <h1> Books List</h1>
+    <ul>
+      <li>
+        <Book name="Acts of Desperation." author="Megan Nolan" />
+      </li>
+      <li>
+        <Book name="Acts of Desperation." author="Megan Nolan" />
+      </li>
+      <li>
+        <Book name="Acts of Desperation." author="Megan Nolan" />
+      </li>
+    </ul>
+
+    <AddBook />
+  </div>
+);
+export default Home;
