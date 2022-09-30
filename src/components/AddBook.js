3554fbcb79a6130c425117dfa18d52c54a6b3012
kidde60 <kgeorgewilliam60@gmail.com>
2022-09-30 23:52:26 +0300

    Add book functionality

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,13 +1,50 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { AddBook } from '../redux/books/books';
 
-const AddBook = () => (
-  <div className="addBook">
-    <h1>Add Books</h1>
-    <form>
-      <input type="text" name="name" placeholder="Tittle" />
-      <input type="text" placeholder="Author" />
-      <button type="submit">Add Book</button>
-    </form>
-  </div>
-);
-export default AddBook;
+const AddBooks = () => {
+  const dispatch = useDispatch();
+  const DisplayBook = () => {
+    const title = document.querySelector('.title').value;
+    const author = document.querySelector('.author').value;
+    if (title !== '' && author !== '') {
+      dispatch(AddBook({ id: uuidv4(), title, author }));
+    }
+  };
+
+  return (
+    <>
+      <form className="form">
+        <h1 className="heading">Add New Book</h1>
+        <div>
+          <input
+            id="temp-id"
+            type="text"
+            name="title"
+            required
+            placeholder="Book title"
+            className="title"
+          />
+          <input
+            type="text"
+            name="author"
+            required
+            placeholder="Author"
+            className="author"
+          />
+          <button
+            className="btn"
+            type="button"
+            onClick={() => {
+              DisplayBook();
+            }}
+          >
+            Add Book
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
+export default AddBooks;
