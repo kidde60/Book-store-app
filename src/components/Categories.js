1315186e1087f708f89524f446dff1f67edb95b5
kidde60 <kgeorgewilliam60@gmail.com>
2022-09-30 23:53:17 +0300

    Add category

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,6 +1,20 @@
-const Categories = () => (
-  <button type="button" className="checkStatus">
-    Check status
-  </button>
-);
-export default Categories;
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import checkStatus from '../redux/categories/categories';
+
+const Category = () => {
+  const Status = useSelector((state) => state.status);
+  const dispatch = useDispatch();
+  const display = () => {
+    dispatch(checkStatus());
+  };
+  return (
+    <>
+      <p>
+        {Status}
+      </p>
+      <button className="btn" onClick={() => display()} type="button">Check Status</button>
+    </>
+  );
+};
+export default Category;
