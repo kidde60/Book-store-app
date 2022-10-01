f5d3dbdf55d07715808f45b5aab83e9b1032d79e
kidde60 <kgeorgewilliam60@gmail.com>
2022-10-01 16:01:39 +0300

    update inport

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import {
-  Route, Routes,
-  Link,
-} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import BookList from './components/BookList';
+import NavBar from './components/NavBar';
 import Category from './components/Category';
 
 const App = () => (
   <div className="wrapper">
-    <div className="header">
-      <h1>Bookstore CMS</h1>
-      <ul className="nav-links">
-        <li className="link"><Link to="/">BOOKS</Link></li>
-        <li className="link"><Link to="/category">CATEGORY</Link></li>
-      </ul>
-    </div>
+    <NavBar />
     <Routes>
       <Route path="/" exact element={<BookList />} />
       <Route path="/category" exact element={<Category />} />
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './index.css';
-// import reportWebVitals from './reportWebVitals';
 import App from './App';
 import store from './redux/configureStore';
 
