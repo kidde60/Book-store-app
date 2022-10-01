abce258dbe054db66360c041eb6e826fe14ff35d
kidde60 <kgeorgewilliam60@gmail.com>
2022-10-01 16:02:45 +0300

    add remove book functionality

--- src/components/Book.js
+++ src/components/Book.js
@@ -11,23 +11,23 @@ const Book = (obj) => {
   return (
     <div className="card">
       <div className="card-1">
-        <p style={{ fontWeight: 600, opacity: 0.5 }}>Action</p>
-        <h2 style={{ marginTop: '-10px', fontWeight: '800' }}>{title}</h2>
-        <p style={{ marginTop: '-15px', color: '#4386bf' }}>{author}</p>
+        <h2>{title}</h2>
+        <p>{author}</p>
         <div className="functions">
           <p>Comments</p>
-          <button type="button" onClick={() => RemoveBtn(id)}>Remove</button>
+          <button className="btn" type="button" onClick={() => RemoveBtn(id)}>Remove</button>
           <p>Edit</p>
         </div>
       </div>
       <div className="card-2">
-        <p className="progress-percent">45%</p>
-        <p className="progress-complete">Completed</p>
+        <p className="percent">45%</p>
+        <p className="completed">Completed</p>
       </div>
+      <div className="hr" />
       <div className="card-3">
-        <p style={{ opacity: 0.5, fontSize: '13px' }}>CURRENT CHAPTER</p>
-        <p className="chapter">CHAPTER 10</p>
-        <button type="button">UPDATE PROGRESS</button>
+        <p>CURRENT CHAPTER</p>
+        <p className="chapter">CHAPTER 12</p>
+        <button className="btn" type="button">UPDATE PROGRESS</button>
       </div>
     </div>
   );
