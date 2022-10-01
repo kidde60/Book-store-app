021a9509f6ecc341096e704e9600da5c056a0b5f
kidde60 <kgeorgewilliam60@gmail.com>
2022-10-01 16:04:00 +0300

    Add navbar

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import checkStatus from '../redux/categories/categories';
+import checkStatus from '../redux/Categories/categories';
 
 const Category = () => {
   const Status = useSelector((state) => state.status);
